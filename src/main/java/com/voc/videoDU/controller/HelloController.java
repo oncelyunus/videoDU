bb7321533d5108092e4835150731ec3dfee8789d
Yunus <yunusoncel@gmail.com>
2019-01-23 23:39:49 +0300

    hello

---
+++ src/main/java/com/voc/videoDU/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.voc.videoDU.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello() {
+        return "Say hello";
+    }
+}
