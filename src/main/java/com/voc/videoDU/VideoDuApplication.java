defdb4f47bb4f397a8fc0bb50809a2f6d6f98b8a
Yunus <yunusoncel@gmail.com>
2019-01-23 22:57:55 +0300

    second commit

---
+++ src/main/java/com/voc/videoDU/VideoDuApplication.java
@@ -0,0 +1,14 @@
+package com.voc.videoDU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoDuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoDuApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/voc/videoDU/VideoDuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.voc.videoDU;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoDuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
