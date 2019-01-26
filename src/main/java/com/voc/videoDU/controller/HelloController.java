c9ecd2c86b0f1422f71ecc3eb066602c92f9d9b0
Yunus <yunusoncel@gmail.com>
2019-01-26 20:52:01 +0300

    .

--- src/main/java/com/voc/videoDU/controller/HelloController.java
+++ src/main/java/com/voc/videoDU/controller/HelloController.java
@@ -7,21 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${spring.data.cassandra.keyspace-name}")
-    private String keyspace;
-
-    @Value("${spring.data.cassandra.contact-points}")
-    private String contact_points;
-
-    @Value("${spring.data.cassandra.password}")
-    private String password;
-
-    @Value("${spring.data.cassandra.username}")
-    private String username;
-
-    @Value("${spring.data.cassandra.port}")
-    private int port;
-
     @GetMapping(value = "/hello")
     public String sayHello() {
         return "Say hello";
